df95b6e0bdf250eb7fed9e9de513266d5c4b7ab7
JiaSing Lin <31025028+anthonyy41@users.noreply.github.com>
2024-01-12 01:40:41 +0800

    - Add global usings for logging, Moq, System.Linq, and Xunit - Delete UnitTest1.cs - Add Moq package reference - Add project reference to WebApiPratice.csproj

---
+++ WebApiPratice.Test/WeatherForecastControllerTests.cs
@@ -0,0 +1,21 @@
+using WebApiPratice.Controllers;
+
+namespace WebApiPratice.Test
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsFiveWeatherForecasts()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
